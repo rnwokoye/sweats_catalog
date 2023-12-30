df40cc031507dfee951bdd2d9f528f86b04d0393
Richard Nwokoye <RNwokoye@gmail.com>
2023-12-30 15:33:03 -0500

    add python connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,23 @@
 import streamlit as st
 import snowflake.connector
+import pandas as pd
 
-
-# my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_data_row = my_cur.fetchone()
-
-st.text("Hello from Snowflake:")
-# st.text(my_data_row)
-
-
-import streamlit
-import snowflake.connector
-import pandas
-
-streamlit.title("Amazing Athleisure Catalog")
+st.title("Amazing Athleisure Catalog")
 # connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
+df = pd.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
 # streamlit.write(df)
 # put the first column into a list
 color_list = df[0].values.tolist()
 # print(color_list)
 # Let's put a pick list here so they can pick the color
-option = streamlit.selectbox("Pick a sweatsuit color or style:", list(color_list))
+option = st.selectbox("Pick a sweatsuit color or style:", list(color_list))
 # We'll build the image caption now, since we can
 product_caption = "Our warm, comfortable, " + option + " sweatsuit!"
 # use the option selected to go back and get all the info from the database
@@ -38,7 +26,6 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
 st.text(my_data_row)
 
 
@@ -49,6 +36,6 @@ my_cur.execute(
 )
 df2 = my_cur.fetchone()
 st.image(df2[0], width=400, caption=product_caption)
-streamlit.write("Price: ", df2[1])
-streamlit.write("Sizes Available: ", df2[2])
-streamlit.write(df2[3])
+st.write("Price: ", df2[1])
+st.write("Sizes Available: ", df2[2])
+st.write(df2[3])
