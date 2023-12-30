fdb283fc3c5de0e3012344e67302b08d3628fdb6
Richard Nwokoye <RNwokoye@gmail.com>
2023-12-30 14:23:52 -0500

    add python connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-# import snowflake.connector
+import snowflake.connector
 
 
 st.header("Hello App")
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
